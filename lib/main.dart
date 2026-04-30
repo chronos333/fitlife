4fc1abf2f8ac0e384ca6095af7d884d0f4c747e8
JustChronos <vinieduardopini.12@gmail.com>
2026-04-30 11:06:19 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,36 @@ import 'package:provider/provider.dart';
 import 'Providers/app_state.dart';
 import 'screens/splash_screen.dart';
 
+/// Função principal da aplicação.
+/// Inicializa o app e injeta o Provider global (AppState)
 void main() {
-  runApp(ChangeNotifierProvider(create: (_) => AppState(), child: MyApp()));
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AppState(), // Cria o estado global da aplicação
+      child: MyApp(), // Widget raiz do app
+    ),
+  );
 }
 
+/// Widget principal da aplicação.
+/// Responsável por configurar o tema e a tela inicial
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Obtém o estado global (Provider)
     var appState = Provider.of<AppState>(context);
 
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: appState.isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      debugShowCheckedModeBanner: false, // Remove banner de debug
+
+      // Define o tema dinamicamente (claro ou escuro)
+      theme: appState.isDarkMode
+          ? ThemeData.dark()
+          : ThemeData.light(),
+
+      // Define a tela inicial do aplicativo
       home: const SplashScreen(),
     );
   }
